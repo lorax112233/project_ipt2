24d24af2bdac69aaa0183be14500de512be14b45
lorax112233 <shadrach.malaque@urios.edu.ph>
2025-09-11 08:27:33 +0800

    Initial commit of project_ipt2

--- resources/js/components/Example.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Example() {
-  return (
-    <div>
-      <h1>Example Page</h1>
-    </div>
-  );
-}
--- routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-// Catch-all route for React
-Route::get('/{any}', function () {
-    return view('home');   // loads resources/views/home.blade.php
-})->where('any', '.*');
